d73ad719c7ffba66543d2984501e230fdfdac988
Andrew Nottoli <nottolivc@gmail.com>
2019-09-10 18:34:10 -0700

    mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
 import React, { useState } from 'react';
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
   const addToSavedList = movie => {
+    /*Prevent Saving Multiples */
+    (!savedList.includes(movie))&& 
+    /* Use spread operator for props */
     setSavedList( [...savedList, movie] );
   };
-
+ /* return the App Class, id returned on one line function with props params */
   return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+
+    <div className="App">
+      <Link to='/SavedList'><SavedList list={savedList} /></Link>
+      <Route path='/' component={MovieList} />
+      <Route path='/movies/:id' render={(props) => <Movie {...props} addToSavedList={addToSavedList} />} />
     </div>
-  );
+  )
 };
 
 export default App;
